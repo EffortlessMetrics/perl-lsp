e05e7c5be318ccb58186d0a5537193e72176cb4c
Steven Zimmerman <git@effortlesssteven.com>
2025-07-18 04:33:45 -0400

    feat(parser): Add support for interpolated strings and enhance double-quoted string parsing

---
+++ test_interpolation_simple.pl
@@ -0,0 +1,4 @@
+my $name = "World";
+my $greeting = "Hello, $name!";
+my $count = 42;
+my $message = "The answer is $count";
---
+++ test_parse_debug.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+use strict;
+
+my $simple = "no interpolation";
+my $name = "World";
+my $interp = "Hello, $name!";
+print $interp;
