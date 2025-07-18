1e86e4c2c0db7886c6887cebd695b3376b1f4e63
Steven Zimmerman <git@effortlesssteven.com>
2025-07-18 11:57:45 -0400

    feat(parser): Add regex and substitution support, enhance grammar and test cases

--- test_comprehensive_final.pl
+++ test_comprehensive_final.pl
@@ -1,52 +1,42 @@
-#!/usr/bin/perl
+#\!/usr/bin/perl
 use strict;
 use warnings;
 
-# Variables and operators
-my $x = 5 + 3 * 2;
-my $str = "Hello" . " World";
-my @range = (1..10);
+# Variables
+my $scalar = 42;
+my @array = (1, 2, 3);
+my %hash = (key => "value");
+
+# String interpolation
+my $name = "World";
+my $greeting = "Hello, $name\!";
+my $array_str = "Array: @array";
+
+# Operators
+my $sum = 10 + 20;
+my $result = $scalar > 10 ? "big" : "small";
 
 # Control flow
-if ($x > 10) {
-    print "Big\n";
-} elsif ($x > 5) {
-    print "Medium\n";
-} else {
-    print "Small\n";
+if ($scalar == 42) {
+    print "The answer\!\n";
 }
 
 # Loops
-for (my $i = 0; $i < 5; $i++) {
-    print "$i ";
-}
-
-foreach my $num (@range) {
-    print "Number: $num\n";
+foreach my $item (@array) {
+    print "Item: $item\n";
 }
 
 # Subroutines
 sub add {
-    my ($a, $b) = @_;
-    return $a + $b;
+    my ($x, $y) = @_;
+    return $x + $y;
 }
 
-# Anonymous sub
-my $multiply = sub {
-    return $_[0] * $_[1];
-};
+# Regular expressions
+if ($greeting =~ /World/) {
+    print "Found World\n";
+}
 
-# Array/hash access
-my @array = (10, 20, 30);
-my %hash = (key => 'value');
-my $elem = $array[1];
-my $val = $hash{key};
+my $regex = qr/pattern/i;
 
-# Method calls
-my $obj = Package->new();
-$obj->method();
-
-# Complex expressions
-my $result = $multiply->(4, 5) + add(1, 2);
-
-1;
+print "Done\!\n";
---
+++ test_regex_simple.pl
@@ -0,0 +1,3 @@
+#!/usr/bin/perl
+my $x = 1;
+if ($x =~ /1/) { print "yes"; }
