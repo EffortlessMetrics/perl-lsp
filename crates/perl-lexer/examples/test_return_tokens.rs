4149fee1b20594d9412e1bec167facedcf301475
Steven Zimmerman <git@effortlesssteven.com>
2025-08-06 18:20:46 -0400

    feat: Update parser to handle return and die statements with modifiers; add tests and improve build profiles

---
+++ crates/perl-lexer/examples/test_return_tokens.rs
@@ -0,0 +1,30 @@
+use perl_lexer::{PerlLexer, TokenType};
+
+fn main() {
+    let test_cases = vec![
+        "return if 1;",
+        "return;",
+        "return $x if $cond;",
+        "return $x or die if $error;",
+    ];
+    
+    for input in test_cases {
+        println!("\nTokenizing: {}", input);
+        let mut lexer = PerlLexer::new(input);
+        
+        loop {
+            match lexer.next_token() {
+                Some(token) => {
+                    println!("  {:?} => '{}'", token.token_type, &input[token.start..token.end]);
+                    if matches!(token.token_type, TokenType::EOF) {
+                        break;
+                    }
+                }
+                None => {
+                    println!("  End of tokens");
+                    break;
+                }
+            }
+        }
+    }
+}
