7bab0575637bf6518958fe510e5faadc4add8cb9
Steven Zimmerman <git@effortlesssteven.com>
2025-08-26 10:21:35 -0400

    style: apply rustfmt formatting

--- crates/perl-parser/src/inline_completions.rs
+++ crates/perl-parser/src/inline_completions.rs
@@ -36,7 +36,7 @@ impl Default for InlineCompletionProvider {
 
 impl InlineCompletionProvider {
     pub fn new() -> Self {
-        Self::default()
+        Self
     }
 
     /// Get inline completions for the given context
